2b92c8a1e3f7cef1caacfc8b4e7d406d6cf66ae6
anthoulab <49809899+anthoulab@users.noreply.github.com>
2019-05-13 10:55:56 +0300

    Update class Employee
    Delete some unnecessary variables and add the isNew method.

--- Server/Employee.java
+++ Server/Employee.java
@@ -1,34 +1,31 @@
 abstract class Employee{
 	protected String username;
 	protected String password;
-	protected String full_name;
-	protected Date birthdate;
-	protected int amka;
-	protected int ama;
-	protected int afm;
-	protected int phone_number;
-	protected String email;
 	protected double hours_worked;
 	protected Device device;
 	boolean is_new;
 
-	Employee(String username, String password, String name, Date date, int amka, int ama, int afm, int number, String email, double hours, boolean is_new){
+	Employee(String username, String password, double hours, boolean is_new){
 		this.username = username;
 		this.password = password;
-		this.full_name = name;
-		this.birthdate = date;
-		this.amka = amka;
-		this.ama = ama;
-		this.afm = afm;
-		this.phone_number = number;
-		this.email = email;
-		this.hoursworked = hours;
+		this.hours_worked = hours;
 		this.is_new = is_new;
 	}
 
-	getDevice(){
+	Device getDevice(){
 
 	}
+	
+	boolean isNew(){
+		if (this.is_new == true){
+			return true;
+		}
+		else{
+			return false;
+		}
+					
+	}
+	
 }
 
 	
